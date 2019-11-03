b1942b4dee5d71efde0380295b0eb1b84ef93652
Ivgenium <genrymorgon@mail.ru>
2019-11-03 19:31:08 +0300

    6.5. Поиск индекса в диапазоне.[#176543]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,9 +1,30 @@
 package ru.job4j.array;
 
 public class FindLoop {
+
     public int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = 0; index != data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * Поиск индекса в диапазоне
+     *
+     * @author Evgeny Vatyutov (genrymorgon@mail.ru)
+     * @param data массив чисел, в котором ведётся поиск
+     * @param el элемент, который нужно найти
+     * @param start индекс, с которого начинаем поиск
+     * @param finish индекс, которым заканчиваем поиск
+     */
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = start; index != finish; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -35,4 +35,26 @@ public class FindLoopTest {
         int expect = 2;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindNothing() {
+        int[] input = new int[] {12, 3, 11, 8, 15};
+        int value = 9;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }

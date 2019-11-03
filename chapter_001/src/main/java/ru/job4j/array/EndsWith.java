838ee6586e1ae4795b22be7c769ac4bfe173979e
Ivgenium <genrymorgon@mail.ru>
2019-11-03 19:08:36 +0300

    6.4.1. Слово заканчивается с ...[#176542]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        //for (int i =)
         int wordLength = word.length;
         int postLength = post.length;
         for (int i = wordLength - 1; i > wordLength - postLength - 1; i--) {

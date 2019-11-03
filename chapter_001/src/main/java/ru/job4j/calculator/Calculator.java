c0f22dae4ccdc6b2b262232d9952fd576de9ad05
Ivgenium <genrymorgon@mail.ru>
2019-11-03 09:10:20 +0300

    5.8. JavaDoc. Code convention[#176524]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,23 +2,63 @@ package ru.job4j.calculator;
 
 import java.sql.SQLOutput;
 
+/**
+ * Class Класс для вычисления арифметических операций + - * /
+ *
+ * @author Evgeny Vatyutov (genrymorgon@mail.ru)
+ * @since 03.11.2019
+ * @version 1
+ */
 public class Calculator {
+
+    /**
+     * Сложение
+     *
+     * @author Evgeny Vatyutov (genrymorgon@mail.ru)
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
+
+    /**
+     * Вычитание
+     *
+     * @author Evgeny Vatyutov (genrymorgon@mail.ru)
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
+
+    /**
+     * Умножение
+     *
+     * @author Evgeny Vatyutov (genrymorgon@mail.ru)
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
+
+    /**
+     * Деление
+     *
+     * @author Evgeny Vatyutov (genrymorgon@mail.ru)
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void subtract(double first, double second) {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
+
     public static void main(String[] args) {
         add(1,1);
         div(4,2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculator;
+
+/**
+ * Package for calculate task.
+ *
+ * @author Evgeny Vatyutov (genrymorgon@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */

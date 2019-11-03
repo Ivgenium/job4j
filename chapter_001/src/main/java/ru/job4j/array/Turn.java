27486183d3fccaa1cb623a18402111769c403efc
Ivgenium <genrymorgon@mail.ru>
2019-11-03 13:30:16 +0300

    6.2. Перевернуть массив.[#176541]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] back(int[] array) {
+        int k, lengthArray = array.length;
+        for (int i = 0; i != lengthArray / 2; i++) {
+            k = lengthArray - 1 - i; // зеркальный индекс
+            int temp1 = array[i];
+            array[i] = array[k];
+            array[k] = temp1;
+        }
+        return array;
+    }
+}

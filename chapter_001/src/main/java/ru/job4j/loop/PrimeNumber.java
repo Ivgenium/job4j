52a54e3e0f8c91fa1bb24bc04e156f800035cd96
Ivgenium <genrymorgon@mail.ru>
2019-11-03 08:45:20 +0300

    5.6. Простые числа.[#176527]. Использован метод из прошлой задачи.

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,14 +4,8 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         for (int i = 2; i <= finish; i++) {
-            boolean prime = true;
-            for (int k = 2; k < i; k++) {
-                if (i %k == 0) {
-                    prime = false;
-                    break;
-                }
-            }
-            if (prime) count++;
+            CheckPrimeNumber prime = new CheckPrimeNumber();
+            if (prime.check(i)) count++;
         }
         return count;
     }

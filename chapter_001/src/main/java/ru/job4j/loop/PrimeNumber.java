0b0ce4de9fc55caf90cbc475ee12f114a7117a13
Ivgenium <genrymorgon@mail.ru>
2019-11-03 11:42:28 +0300

    5.6. Простые числа.[#176527]. Исправление.

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
+        CheckPrimeNumber prime = new CheckPrimeNumber();
         for (int i = 2; i <= finish; i++) {
-            CheckPrimeNumber prime = new CheckPrimeNumber();
             if (prime.check(i)) count++;
         }
         return count;

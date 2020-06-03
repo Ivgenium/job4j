b7faa41f7176029614de9bb07330967ef7b4287f
Ivgenium <genrymorgon@mail.ru>
2020-06-03 22:06:41 +0300

    2.2. Рефакторинг - Площадь треугольника.[122642#176647]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,61 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
-        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    private Point first;
+    private Point second;
+    private Point third;
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     *
+     * Формула.
+     *
+     * (a + b + c)
+     *
+     * @return Периметр.
+     */
+    public double period() {
+        return first.distance(second) + second.distance(third) + third.distance(first);
+    }
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * Формула.
+     *
+     * √ p *(p - ab) * (p - ac) * (p - bc)
+     *
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     *
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = second.distance(third);
+        double c = third.distance(first);
+        double p = this.period() / 2;
+        if (this.exist(a, b, c)) {
+            rsl = sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     *
+     * @param a Длина от точки a b.
+     * @param b Длина от точки a c.
+     * @param c Длина от точки b c.
+     * @return Вернуть true, если треугольник существует или false.
+     */
+    private boolean exist(double a, double c, double b) {
+        return a + b > c && a + c > b && b + c > a;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,18 +1,27 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void period() {
+        Point a = new Point(2, -1);
+        Point b = new Point(-1, 3);
+        Point c = new Point(2, 7);
+        double expected = 18;
+        Triangle test = new Triangle(a, b, c);
+        double out = test.period();
+        Assert.assertEquals(expected, out, 0.01);
     }
     @Test
-    public void whenNotExist() {
-        boolean result = Triangle.exist(1.0, 1.0, 3.0);
-        assertThat(result, is(false));
+    public void area() {
+        Point a = new Point(2, -1);
+        Point b = new Point(-1, 3);
+        Point c = new Point(2, 7);
+        double expected = 12;
+        Triangle test = new Triangle(a, b, c);
+        double out = test.area();
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
